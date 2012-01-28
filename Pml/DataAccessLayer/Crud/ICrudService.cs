5abca6b65f6333cc34c01ed6726f28cc2e34520d
daub815@gmail.com <daub815@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-01-28 19:25:14 +0000

    Dependency Injection implementation, so the callers don't care what DataAccessLayer they are using.

---
+++ Pml/DataAccessLayer/Crud/ICrudService.cs
@@ -0,0 +1,34 @@
+﻿namespace Sweng500.Pml.DataAccessLayer
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides the interface for the CRUD functionality
+    /// </summary>
+    public interface ICrudService
+    {
+        /// <summary>
+        /// Gets the list of books in the inventory
+        /// </summary>
+        /// <returns>The list of books</returns>
+        IEnumerable<Book> GetBooks();
+
+        /// <summary>
+        /// Updates the provided book
+        /// </summary>
+        /// <param name="book">The updated book</param>
+        void Update(Book book);
+
+        /// <summary>
+        /// Adds the provided book
+        /// </summary>
+        /// <param name="book">The book to add</param>
+        void Add(Book book);
+
+        /// <summary>
+        /// Deletes the provided book
+        /// </summary>
+        /// <param name="book">The book to delete</param>
+        void Delete(Book book);
+    }
+}
