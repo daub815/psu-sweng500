49378d1288f07e56c9a3f4a291c28ea04e8e67cb
daub815@gmail.com <daub815@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-01-28 16:11:17 +0000

    Changed sql compact to 3.5 (due to visual studio incompatibility) Added ICrudService interface Added Database

---
+++ trunk/Pml/DataAccessLayer/ICrudService.cs
@@ -0,0 +1,12 @@
+﻿namespace Sweng500.Pml.DataAccessLayer
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides the interface for the CRUD functionality
+    /// </summary>
+    public interface ICrudService
+    {
+        IEnumerable<Book> GetBooks();
+    }
+}
