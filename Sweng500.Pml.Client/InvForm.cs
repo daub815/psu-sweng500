e1e7cd31747ae1c3da588bcc0c86aad165aec91e
bobneudecker@gmail.com <bobneudecker@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-01-29 20:56:42 +0000

    add client project

---
+++ Sweng500.Pml.Client/InvForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Sweng500.Pml.DataAccessLayer;
+
+
+namespace Sweng500.Pml.Client
+{
+    public partial class InvForm : Form
+    {
+        public InvForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+ 
+
+        private void inventoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void displayInvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeInv_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dispInv_Click(object sender, EventArgs e)
+        {
+            // Get the Crud Service via Dependency Injection
+            var service = Repository.Instance.ServiceLocator.GetInstance<ICrudService>();
+            // Get a list of books
+            var books = service.GetBooks();
+        }
+    }
+}
