dfd2451ac66f1ea2ae0ad64eec5904e476b75647
bobneudecker@gmail.com <bobneudecker@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-02-11 21:59:26 +0000

    Temp code to use a TMedia and TVideo and instantiate  TheCollection with TMedia memebers.

---
+++ Pml/ViewModel/TVideo.cs
@@ -0,0 +1,20 @@
+﻿namespace Sweng500.Pml.ViewModel
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    public class TVideo : TMedia
+    {
+        /// <summary>
+        /// Temporary Board Rating
+        /// </summary>
+        public string BoardRating { get; set; }
+
+        /// <summary>
+        /// TEmporary Director only using name
+        /// </summary>
+        public string DirectorName { get; set; }
+    }
+}
