3efd80a42ed4843895c018b3e19045d999c8053a
daub815@gmail.com <daub815@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-02-16 01:57:16 +0000

    Initial cut with some groundwork.

--- trunk/Pml/ViewModel/Workspaces/MainWorkspaceViewModel.cs
+++ trunk/Pml/ViewModel/Workspaces/MainWorkspaceViewModel.cs
@@ -8,73 +8,41 @@
     /// </summary>
     public class MainWorkspaceViewModel : WorkspaceViewModel
     {
-        /// <summary>
-        /// The property name for the TempEditor property
-        /// </summary>
-        public const string TempEditorPropertyName = "TempEditor";
+        #region Statics
 
-        /// <summary>
-        /// The backing field for the TempEditor property
-        /// </summary>
-        private EditWorkspaceViewModel mTempEditor = new EditWorkspaceViewModel();
+        #endregion Statics
+
+        #region Fields
+
+        #endregion Fields
+
+        #region Constructors
 
         /// <summary>
         /// Initializes a new instance of the MainWorkspaceViewModel class
         /// </summary>
         public MainWorkspaceViewModel()
+            : base("Personal Media Library")
         {
-            this.Name = "Personal Media Library";
-            TMedia media1 = new TMedia("bob to nowhere", 3663, "This is a book about a trip to no-where");
-            media1.Publisher = "nice publisher";
-            TMedia media2 = new TMedia("another", 736, "just another book");
-
-            TVideo video1 = new TVideo();
-            video1.Title = "Amazing Trip";
-            video1.Publisher = "videoPub";
-            video1.BoardRating = "PG-13";
-            video1.DirectorName = "Frank Capra";
-            video1.MediaDescription = "thrilling exploit in 3D";
-
-            // Create the collection
-            this.TheCollection = new ObservableCollection<TMedia>();
-            this.TheCollection.Add(media1);
-            this.TheCollection.Add(media2);
-            this.TheCollection.Add(video1);
-
-            // Create the collection
-//            this.TheCollection = new ObservableCollection<string>();
-
-//            this.TheCollection.Add(Guid.NewGuid().ToString());
-//            this.TheCollection.Add("here is my test item");
-//            this.TheCollection.Add(Guid.NewGuid().ToString());
+            this.Workspaces = new ObservableCollection<WorkspaceViewModel>
+            {
+                new InventoryWorkspaceViewModel()
+            };
         }
 
+        #endregion Constructors
+
+        #region Properties
+
         /// <summary>
-        /// Gets or sets the collection
+        /// Gets a collection of workspaces
         /// </summary>
-        /// <remarks>No need to raise this property changed event because we don't do a swap of values.  We modify this one.</remarks>
-        public ObservableCollection<TMedia> TheCollection
+        public ObservableCollection<WorkspaceViewModel> Workspaces
         {
             get;
-            set;
+            protected set;
         }
 
-        /// <summary>
-        /// Gets or sets the temporary editor
-        /// </summary>
-        public EditWorkspaceViewModel TempEditor
-        {
-            get
-            {
-                return this.mTempEditor;
-            }
-
-            set
-            {
-                var oldValue = this.mTempEditor;
-                this.mTempEditor = value;
-                this.RaisePropertyChanged(TempEditorPropertyName);
-            }
-        }
+        #endregion Properties
     }
 }
