3fd82f21fd882d177b56f3df83d22929090778bb
bobneudecker@gmail.com <bobneudecker@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-02-20 00:43:56 +0000

    Update EntityCrudServiceTest.cs to add test for videos and GetGenre.  DB for Code gets key overwritten when adding through EntityFramework

--- Pml/Test/Unit/EntityCrudServiceTest.cs
+++ Pml/Test/Unit/EntityCrudServiceTest.cs
@@ -40,9 +40,7 @@
                 {
                     context.Media.DeleteObject(media);
                     context.SaveChanges();
-                }
-
-               
+                }  
             }
             catch (Exception e)
             {
@@ -79,15 +77,13 @@
         }
 
         /// <summary>
-        /// A test for Add
+        /// A test for Add of a book
         /// </summary>
         [TestMethod]
         public void AddTest()
         {
             var originalBooks = Mock.MediaObjectMother.CreateNewBooks();
-            var booksToAdd = new List<Book>(originalBooks);
-
-            var originalVideos = Mock.MediaObjectMother.CreateNewVideos();
+            var booksToAdd = Mock.MediaObjectMother.CreateNewBooks();
 
             // Create a set of books (original and one to add), but both will be the same
             var testBooks = originalBooks.Zip(booksToAdd, (a, b) => new { Original = a, ToAdd = b });            
@@ -99,15 +95,49 @@
                 testBook.ToAdd.Acquired = testBook.Original.Acquired;
 
                 // Add the book
-                var addedBook = service.Add(testBook.ToAdd);
+                Book addedBook = (Book)service.Add(testBook.ToAdd);
                 
                 // Verify the book content
                 Assert.IsNotNull(addedBook);
                 Assert.IsNotNull(testBook.Original);
+                Assert.IsTrue(testBook.Original.BookID == 0);
+                Assert.IsTrue(testBook.Original.BookID != addedBook.BookID);
+                Assert.IsTrue(testBook.Original.MediaID == 0);
                 Assert.IsTrue(testBook.Original.MediaID != addedBook.MediaID);
                 Assert.IsTrue(testBook.Original.Title == addedBook.Title);
 
                 Assert.IsTrue(testBook.Original.Acquired == addedBook.Acquired);
+            }
+        }
+
+        /// <summary>
+        /// A test for Add of a video
+        /// </summary>
+        [TestMethod]
+        public void AddVideoTest()
+        {
+            var originalVideos = Mock.MediaObjectMother.CreateNewVideos();
+            var videosToAdd = Mock.MediaObjectMother.CreateNewVideos();
+
+            // Create a set of videos (original and one to add), but both will be the same
+            var testVideos = originalVideos.Zip(videosToAdd, (a, b) => new { Original = a, ToAdd = b });
+
+            EntityCrudService service = new EntityCrudService();
+            foreach (var testMedia in testVideos)
+            {
+                //// Add the video
+                Video addedMedia = (Video)service.Add(testMedia.ToAdd);
+
+                //// Verify the book content
+                Assert.IsNotNull(addedMedia);
+                Assert.IsNotNull(testMedia.Original);
+                Assert.IsTrue(testMedia.Original.VideoID == 0);
+                Assert.IsTrue(testMedia.Original.VideoID != addedMedia.VideoID);
+                Assert.IsTrue(testMedia.Original.MediaID == 0);
+                Assert.IsTrue(testMedia.Original.MediaID != addedMedia.MediaID);
+                Assert.IsTrue(testMedia.Original.Title == addedMedia.Title);
+
+                Assert.IsTrue(testMedia.Original.Acquired == addedMedia.Acquired);
             }
         }
 
@@ -147,7 +177,7 @@
         }
 
         /// <summary>
-        /// A test for GetBooks
+        /// A test for GetMediaItems that is specific to books
         /// </summary>
         [TestMethod]
         public void GetBooksTest()
@@ -173,6 +203,46 @@
             // They should not match
             expected.Add(new Book());
             Assert.IsFalse(actual.Intersect(expected).Count() == expected.Count);
+        }
+
+        /// <summary>
+        /// A test for GetMediaItems specific to testing videos
+        /// </summary>
+        [TestMethod]
+        public void GetVideosTest()
+        {
+            var service = new EntityCrudService();
+
+            // Add the list of books to get
+            var expected = new List<Video>();
+            int videoCount = 0;
+            int otherCount = 0;
+
+            foreach (var item in Mock.MediaObjectMother.CreateNewVideos())
+            {
+                var itemAdded = service.Add(item);
+                Assert.IsNotNull(itemAdded);
+                if (itemAdded is Video)
+                {
+                    expected.Add((Video)itemAdded);
+                }
+            }
+
+            // They should match
+            var actual = service.GetMediaItems();
+            foreach (var item in actual)
+            {
+                if (item is Video)
+                {
+                    videoCount++;
+                }
+                else
+                {
+                    otherCount++;
+                }
+            } 
+         
+            Assert.IsTrue(videoCount == expected.Count);
         }
 
         /// <summary>
@@ -220,6 +290,76 @@
                 Assert.IsTrue(entry.FromService.Equals(entry.TestUpdate));
                 Assert.IsTrue(entry.FromService.Title == entry.TestUpdate.Title);
             }
+        }
+
+        /// <summary>
+        /// A test for GetGenre
+        /// </summary>
+        [TestMethod]
+        public void GetGenres()
+        {
+            MasterEntities context = null;
+            this.ClearCodes();
+            try
+            {
+                context = new MasterEntities();
+                Code genre1 = new Code();
+                genre1.CodeTypeID = 1;
+                genre1.CodeID = 1;
+                genre1.CodeDescription = "test 1";
+
+                context.Code.AddObject(genre1);
+
+                Code genre2 = new Code();
+                genre2.CodeTypeID = 1;
+                genre2.CodeID = 2;
+                genre2.CodeDescription = "test 2";
+                context.Code.AddObject(genre2);
+
+                Code format1 = new Code();
+                format1.CodeTypeID = 2;
+                format1.CodeID = 1;
+                format1.CodeDescription = "test format 1";
+                context.Code.AddObject(format1);
+
+                Code format2 = new Code();
+                format2.CodeTypeID = 2;
+                format2.CodeID = 2;
+                format2.CodeDescription = "test format 2";
+                context.Code.AddObject(format2);
+
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                string message = "Unable to clear the database after test.";
+                message = message + "Received Exception: " + e.Message;
+
+                Assert.Fail(message);
+            }
+
+            var service = new EntityCrudService();
+            IDictionary<int, string> genres = service.GetGenres();
+            Assert.IsTrue(genres.Count == 2);
+            ICollection<string> descriptions = genres.Values;
+            Assert.IsTrue(descriptions.Contains("test 1"));
+            Assert.IsTrue(descriptions.Contains("test 2"));
+            this.ClearCodes(); 
+        }
+
+        /// <summary>
+        /// clear codes in database
+        /// </summary>
+        public void ClearCodes()
+        {
+            var context = new MasterEntities();
+
+            foreach (var code in context.Code)
+            {
+                context.Code.DeleteObject(code);
+            }
+
+            context.SaveChanges();
         }
     }
 }
