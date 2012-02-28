7f92606df9b3d3387ff7bd4f57ef4ff93c3283ff
daub815@gmail.com <daub815@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-02-28 00:55:57 +0000

    Added MainWorkspaceViewModelTest

---
+++ Pml/Test/Unit/MainWorkspaceViewModelTest.cs
@@ -0,0 +1,69 @@
+﻿namespace Sweng500.Pml.Test.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Sweng500.Pml.ViewModel;
+    using Sweng500.Pml.ViewModel.Workspaces;    
+    
+    /// <summary>
+    ///This is a test class for MainWorkspaceViewModelTest and is intended
+    ///to contain all MainWorkspaceViewModelTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MainWorkspaceViewModelTest
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// A test for MainWorkspaceViewModel Constructor
+        /// </summary>
+        [TestMethod()]
+        public void MainWorkspaceViewModelConstructorTest()
+        {
+            MainWorkspaceViewModel target = new MainWorkspaceViewModel();
+            Assert.IsNotNull(target.CloseWorkspaceCommand);
+            Assert.IsNotNull(target.SelectedWorkspace);
+            
+            // Verify workspaces
+            Assert.IsNotNull(target.Workspaces);
+            Assert.IsTrue(target.Workspaces.Count == 1);
+            Assert.IsNotNull(target.Workspaces[0]);
+            Assert.IsInstanceOfType(target.Workspaces[0], typeof(InventoryWorkspaceViewModel));
+
+            // Verify commands are set
+            Assert.IsNotNull(GlobalCommands.Instance.AddMediaItemCommand);
+            Assert.IsNotNull(GlobalCommands.Instance.EditMediaItemCommand);
+        }
+
+        /// <summary>
+        ///A test for SelectedWorkspace
+        ///</summary>
+        [TestMethod()]
+        public void SelectedWorkspaceTest()
+        {
+            MainWorkspaceViewModel target = new MainWorkspaceViewModel(); // TODO: Initialize to an appropriate value
+            
+            // Verify auto select works
+            WorkspaceViewModel actual;
+            actual = target.SelectedWorkspace;
+            Assert.IsNotNull(actual);
+            Assert.ReferenceEquals(actual, target.Workspaces[0]);
+
+            // Verify selected workspace with an added workspace
+            InventoryWorkspaceViewModel workspace = new InventoryWorkspaceViewModel();
+            workspace.Name = "Second Inventory";
+            target.Workspaces.Add(workspace);
+            workspace.IsSelected = true;
+            actual = target.SelectedWorkspace;
+            Assert.IsNotNull(actual);
+            Assert.ReferenceEquals(actual, workspace);
+        }
+    }
+}
