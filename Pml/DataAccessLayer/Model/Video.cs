1b6e45d0ffed6251173872d369a37c4dbbf8c99d
daub815@gmail.com <daub815@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-03-03 23:31:02 +0000

    Added equals method to Person class. Added Add/Remove Person method to EditWorkspaceViewModel. Added convenience methods to the Book and Video classes for adding people.

--- Pml/DataAccessLayer/Model/Video.cs
+++ Pml/DataAccessLayer/Model/Video.cs
@@ -16,5 +16,76 @@
             this.Acquired = DateTime.Now;
             this.Released = DateTime.Now;
         }
+
+        /// <summary>
+        /// Provides a method to determine if this person is associated with the Directors or Producers collection of this book
+        /// </summary>
+        /// <param name="person">The person to check</param>
+        /// <returns>A value indicating if this person is associated with the Directors or Producers collection of this book</returns>
+        public override bool ContainsPerson(Person person)
+        {
+            bool rtn = false;
+
+            if (person is Director)
+            {
+                rtn = this.Directors.Contains((Director)person);
+            }
+            else if (person is Producer)
+            {
+                rtn = this.Producers.Contains((Producer)person);
+            }
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// Provides a way to add a person depending on the type of person to the correct collection
+        /// </summary>
+        /// <param name="person">The Person to add</param>
+        /// <returns>A value indicating if it was successfully added to a collection</returns>
+        public override bool AddPerson(Person person)
+        {
+            bool rtn = false;
+
+            if (true == this.ContainsPerson(person))
+            {
+                if (person is Producer)
+                {
+                    this.Producers.Add((Producer)person);
+                    rtn = true;
+                }
+                else if (person is Director)
+                {
+                    this.Directors.Add((Director)person);
+                    rtn = true;
+                }
+            }
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// Provides a way to remove a person depending on the type of person from the correct collection
+        /// </summary>
+        /// <param name="person">The person to remove</param>
+        /// <returns>A value indicating whether it was successfully found and removed from the collection</returns>
+        public override bool RemovePerson(Person person)
+        {
+            bool rtn = false;
+
+            if (true == this.ContainsPerson(person))
+            {
+                if (person is Producer)
+                {
+                    rtn = this.Producers.Remove((Producer)person);
+                }
+                else if (person is Director)
+                {
+                    rtn = this.Directors.Remove((Director)person);
+                }
+            }
+
+            return rtn;
+        }
     }
 }
