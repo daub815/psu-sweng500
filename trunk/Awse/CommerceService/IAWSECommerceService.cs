581afff4ba4c9404319bb628579e380f6e9c46fe
bobneudecker@gmail.com <bobneudecker@gmail.com@80bdda37-f6bc-e606-795a-f5d6f11159e6>
2012-03-19 11:27:04 +0000

    create interface for AWSECommerceService

---
+++ trunk/Awse/CommerceService/IAWSECommerceService.cs
@@ -0,0 +1,19 @@
+﻿namespace Sweng500.Awse.CommerceService
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface that defines an ItemSearch for the Amazon ProfuctAdvertising API
+    /// </summary>
+    public interface IAWSECommerceService
+    {
+        /// <summary>
+        /// A method to do an item search using the Amazon web service
+        /// </summary>
+        /// <param name="searchIndex"> the group to do search such as "DVD","Books"</param>
+        /// <param name="title">words in the title to search on</param>
+        /// <param name="keywords">keywords to search on</param>
+        /// <returns>a list of responses that are defined parts of the search response</returns>
+        IList<ItemResponse> ItemSearch(string searchIndex, string title, string keywords);
+    }
+}
